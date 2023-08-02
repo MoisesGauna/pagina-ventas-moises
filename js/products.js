8ee4e0bb12083f5e8babd36fd56fced44ad06f0b
MoisesGauna <moises.a.gauna@gmail.com>
2023-08-02 12:38:31 -0300

    first commit

---
+++ js/products.js
@@ -0,0 +1,112 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Asus RTX3060" ,
+        price: 270000,
+        quanty: 1,
+        img: "./media/placa1.jpg"
+    },
+    {
+        id: 1,
+        productName: "Asus RTX3060" ,
+        price: 270000,
+        quanty: 1,
+        img: "./media/placa1.jpg"
+    },
+    {
+        id: 1,
+        productName: "Asus RTX3060" ,
+        price: 270000,
+        quanty: 1,
+        img: "./media/placa1.jpg"
+    },
+    {
+        id: 1,
+        productName: "Asus RTX3060" ,
+        price: 270000,
+        quanty: 1,
+        img: "./media/placa1.jpg"
+    },
+    {
+        id: 1,
+        productName: "Asus RTX3060" ,
+        price: 270000,
+        quanty: 1,
+        img: "./media/placa1.jpg"
+    },
+    {
+        id: 1,
+        productName: "Asus RTX3060" ,
+        price: 270000,
+        quanty: 1,
+        img: "./media/placa1.jpg"
+    },
+    {
+        id: 1,
+        productName: "Asus RTX3060" ,
+        price: 270000,
+        quanty: 1,
+        img: "./media/placa1.jpg"
+    },    {
+        id: 1,
+        productName: "Asus RTX3060" ,
+        price: 270000,
+        quanty: 1,
+        img: "./media/placa1.jpg"
+    },    {
+        id: 1,
+        productName: "Asus RTX3060" ,
+        price: 270000,
+        quanty: 1,
+        img: "./media/placa1.jpg"
+    },
+    {
+        id: 2,
+        productName: "placa2",
+        price: 1100,
+        quanty: 1,
+        img: "./media/placa2.jpg"
+    },
+    {
+        id: 3,
+        productName: "placa3",
+        price: 1200,
+        quanty: 1,
+        img: "./media/placa3.jpg"
+    },
+    {
+        id: 4,
+        productName: "placa4",
+        price: 1200,
+        quanty: 1,
+        img: "./media/placa3.jpg"
+    },
+    {
+        id: 5,
+        productName: "placa5",
+        price: 1200,
+        quanty: 1,
+        img: "./media/placa3.jpg"
+    },
+    {
+        id: 6,
+        productName: "placa6",
+        price: 1200,
+        quanty: 1,
+        img: "./media/placa3.jpg"
+    },
+    {
+        id: 7,
+        productName: "placa7",
+        price: 1200,
+        quanty: 1,
+        img: "./media/placa3.jpg"
+    },
+    {
+        id: 8,
+        productName: "placa8",
+        price: 1200,
+        quanty: 1,
+        img: "./media/placa3.jpg"
+    },
+];
